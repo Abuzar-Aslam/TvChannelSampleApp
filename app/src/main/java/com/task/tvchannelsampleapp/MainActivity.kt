552d351837c4d417242362e9344d33b7f6155b64
abuzar_maxis <abuzar@maxis.com.my>
2020-08-22 20:25:00 +0800

    Initial commit

---
+++ app/src/main/java/com/task/tvchannelsampleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.task.tvchannelsampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
